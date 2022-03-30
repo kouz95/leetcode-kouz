a67c28524b07a20cdfaeef13d853beb7ddcd3f14
Gyeongjun Kim <lastpuzly@gmail.com>
2022-03-30 23:29:14 +0900

    Time: 12 ms (46.76%), Space: 14.9 MB (43.99%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -11,15 +11,21 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-    
-        if (list1 == nullptr)
+        if (list1 == nullptr) {
             return list2;
+        }
 
-        if (list2 == nullptr)
+        if (list2 == nullptr) {
             return list1;
+        }
 
         if (list1->val < list2->val) {
             list1->next = mergeTwoLists(list1->next, list2);
+            return list1;
+        } else if (list1->val == list2->val) {
+            ListNode *pNode = list1->next;
+            list1->next = list2;
+            list2->next = mergeTwoLists(pNode, list2->next);
             return list1;
         } else {
             list2->next = mergeTwoLists(list1, list2->next);
