36f556a3e4b1aec3e5f7b39cc1cbffef8b6f4fbf
Gyeongjun Kim <lastpuzly@gmail.com>
2022-06-01 22:48:30 +0900

    Time: 179 ms (12.98%), Space: 214 MB (69.23%) - LeetHub

--- 412-fizz-buzz/412-fizz-buzz.rb
+++ 412-fizz-buzz/412-fizz-buzz.rb
@@ -1,26 +1,10 @@
 # @param {Integer} n
 # @return {String[]}
 def fizz_buzz(n)
-    res = []
-    
-    (1..n).each do |i|
-        if i % 15 == 0
-            res.push('FizzBuzz')
-            next
-        end
-        
-        if i % 3 == 0
-            res.push('Fizz')
-            next
-        end
-        
-        if i % 5 == 0
-            res.push('Buzz')
-            next
-        end
-        
-        res.push(i.to_s)
+    (1..n).map do |i|
+        next 'FizzBuzz' if (i % 15).zero?
+        next 'Buzz' if (i % 5).zero?
+        next 'Fizz' if (i % 3).zero?
+        i.to_s
     end
-    
-    res
 end
