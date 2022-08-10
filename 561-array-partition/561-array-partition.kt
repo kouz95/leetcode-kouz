f3384d4fc3aeec5ba9cc7f077dfe190ff9cb8273
Gyeongjun Kim <lastpuzly@gmail.com>
2022-08-11 00:17:08 +0900

    Time: 731 ms (43.86%), Space: 76.1 MB (5.26%) - LeetHub

--- 561-array-partition/561-array-partition.kt
+++ 561-array-partition/561-array-partition.kt
@@ -1,7 +1,6 @@
 class Solution {
-    fun arrayPairSum(nums: IntArray): Int {
-        return nums.sortedArrayDescending()
-            .filterIndexed { index, _ -> index % 2 != 0 }
-            .sum()
-    }
+    fun arrayPairSum(nums: IntArray): Int =
+    nums.sortedArrayDescending()
+    .filterIndexed {index, _ -> index % 2 != 0}
+    .sum()
 }
