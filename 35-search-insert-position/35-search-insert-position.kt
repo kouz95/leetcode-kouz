15bc52b1501f6b80103002358e951fa6fa37a1ef
Gyeongjun Kim <lastpuzly@gmail.com>
2022-08-24 23:14:44 +0900

    Time: 354 ms (15.54%), Space: 37.2 MB (87.18%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.kt
+++ 35-search-insert-position/35-search-insert-position.kt
@@ -1,11 +1,11 @@
 class Solution {
     fun searchInsert(nums: IntArray, target: Int): Int {
         var rightIndex = nums.size - 1
-        var leftIndex = 0;
-
+        var leftIndex = 0
+        
         while (leftIndex < rightIndex) {
             val middleIndex = (leftIndex + rightIndex) / 2
-
+            
             if (nums[middleIndex] == target) {
                 return middleIndex
             }
@@ -15,7 +15,7 @@ class Solution {
             }
             leftIndex = middleIndex + 1
         }
-
-        return if (nums[leftIndex] < target) leftIndex + 1 else leftIndex   
+        
+        return if (nums[leftIndex] < target) leftIndex + 1 else leftIndex
     }
 }
